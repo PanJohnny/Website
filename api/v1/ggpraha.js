acfd5c6ce17b1ca97964b457e2f5825de52eb106
PanJohnny <devstefancz@gmail.com>
2022-01-29 15:09:43 +0100

    bug fixes

---
+++ api/v1/ggpraha.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    const id = req.header("id")
+    const secret = req.header("secret")
+    if (!id) {
+        res.status(400).json({ reason: "missing id" })
+        return
+    } else if (!secret) {
+        res.status(400).json({ reason: "missing secret" })
+        return
+    }
+    const json = JSON.parse(fs.readFileSync("./data/ggpraha.json", "utf8"))
+
+    const field = json.find(item => item.lampa == id && item.secret == secret)
+    if (!field) {
+        res.status(400).json({ reason: "invalid id or secret" })
+        return
+    }
+    res.json(
+        {
+            "id": field.lampa,
+            "name": field.lokace,
+            "description": field.popis,
+            "point": field.map_link
+        }
+    )
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express()
 const redoc = require("redoc-express")
 const fs = require("fs");
+const ggpraha = require(__dirname + "/api/v1/ggpraha.js")
+
+app.use(express.json({ extended: false }))
+app.use("/api/v1/ggpraha", ggpraha)
 
 app.get('/docs/swagger.yml', (req, res) => {
-    res.send(fs.readFileSync("./swagger.yml", "utf8"));
+    res.sendFile(__dirname + '/swagger.yml')
 });
 
 // define title and specUrl location
@@ -20,34 +24,5 @@ app.get(
 app.get("/", (req, res) => {
     res.redirect("/docs");
 })
-
-app.get('/api/v1/ggpraha', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    const id = req.header("id")
-    const secret = req.header("secret")
-    if(!id) {
-        res.status(400).json({reason: "missing id"})
-        return
-    } else if(!secret) {
-        res.status(400).json({reason: "missing secret"})
-        return
-    }
-    const json = JSON.parse(fs.readFileSync("./data/ggpraha.json", "utf8"))
-
-    const field = json.find(item => item.lampa == id && item.secret == secret)
-        if(!field) {
-            res.status(400).json({reason: "invalid id or secret"})
-            return
-        }
-        res.json(
-            {
-                "id": field.lampa,
-                "name": field.lokace,
-                "description": field.popis,
-                "point": field.map_link
-            }
-        )
-    }
-)
 
 app.listen(process.env.PORT || 8080, () => console.log(`Loaded!`))
