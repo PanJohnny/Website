d1817c441039640bb29cbef0bfd999664eb580c6
PanJohnny <devstefancz@gmail.com>
2022-02-11 21:59:40 +0100

    made the page better

--- index.js
+++ index.js
@@ -31,8 +31,16 @@ app.get("/assets/flower.svg", (req, res) => {
     res.sendFile(__dirname + "/assets/flower.svg")
 })
 
+app.get("/assets/kilohard.png", (req, res) => {
+    res.sendFile(__dirname + "/assets/kilohard.png")
+})
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/html/index.html")
+})
+
+app.get("/style.css", (req, res) => {
+    res.sendFile(__dirname + "/style.css")
 })
 
 app.listen(process.env.PORT || 8080, () => console.log(`Loaded!`))
