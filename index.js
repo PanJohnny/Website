463c8dda6daf053eb1a75db650eca6d4d177277f
PanJohnny <devstefancz@gmail.com>
2022-02-12 10:45:29 +0100

    Updated stuff

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const redoc = require("redoc-express")
 const ggpraha = require(__dirname + "/api/v1/ggpraha.js")
 const cors = require("cors")
+const fetch = require("node-fetch")
 
 
 app.use(cors())
@@ -41,6 +42,33 @@ app.get("/", (req, res) => {
 
 app.get("/style.css", (req, res) => {
     res.sendFile(__dirname + "/style.css")
+})
+
+app.post("/feedback", async (req, res) => {
+    // get json in body and send it to discord webhook
+    const { name, email, message } = req.body
+    const discordWebhook = 'https://discord.com/api/webhooks/941982810368004158/h-lW0ro4xLH4eYC9kM3q0i2sW_BUYKZTIAMPZYCJQ0hS4tubvgn3jwVWrmMY-YgjPQfz'
+    fetch(
+        discordWebhook,
+        {
+            method: 'post',
+            headers: {'Content-Type': 'application/json',},
+            body: JSON.stringify({
+                // the username to be displayed
+                username: name,
+                // the avatar to be displayed
+                avatar_url: "",
+                // send email and messagw
+                content: `__New Feedback__\n\n**Email:** ${email}\n**Message:** ${message}` 
+            }),
+        }
+    ).catch(arr => console.error(arr))
+
+    res.status(200).send("Feedback received")
+})
+
+app.get("/script.js", (req, res) => {
+    res.sendFile(__dirname + "/script.js")
 })
 
 app.listen(process.env.PORT || 8080, () => console.log(`Loaded!`))
